2c0b6bba36ad19986a6f217dbb2e4e0da6c3d90e
rams0003 <s12977.rakshith@gmail.com>
2023-09-22 15:26:13 +0530

    practice programs

---
+++ app/src/main/java/day1/Practice.kt
@@ -0,0 +1,106 @@
+package day1
+
+class Practice {
+}
+
+//fun main() {
+//    val name = "Rakshith"
+//    println(name)  // Rakshith
+
+    // fun main(args: Array<String>)
+   // println(args.contentToString())  // []
+
+   // println("Hello World!") // Hello World
+   // println(42)  //42  Output will be in separate lines
+
+   // print("Sum of 4 n 5 is ")
+   // println(sum(4,5))
+
+//    print("Sum of 55 n 44 = ")
+//    println(sum1(55,44))
+
+//}
+
+//fun sum(a: Int,b: Int): Int {
+//    return a + b  // Sum of 4 n 5 is 9
+//}
+
+//fun sum1(a: Int, b: Int):Int {
+//    return a + b  // Sum of 55 n 44 = 99
+//}
+//or
+//fun sum1(a:Int, b:Int):Int = a + b // Doing it in same line
+
+
+// ------------------------------------------------
+
+//fun main() {
+//    printSum(-1,8)
+//}
+
+//fun printSum(a: Int, b: Int):Int {
+//    println("Sum of $a and $b is ${a+b}")  // Sum of -1 and 8 is 7
+//    return a + b
+//}
+
+//fun printSum(a: Int, b: Int): Unit {  //Unit is return type
+//    println("Sum of $a and $b is ${a+b}")  // Sum of -1 and 8 is 7
+//}
+
+//fun printSum(a: Int, b: Int) {  //Ommitting the Unit , then also we will get the output
+//    println("Sum of $a and $b is ${a+b}")  // Sum of -1 and 8 is 7
+//}
+
+
+//val pi = 3.14
+//var a = 0
+//
+//fun increment() {
+//    a +=1
+//}
+//fun main() {
+//    println("$pi and $a")  // 3.14 and 0
+//
+//    increment()
+//    println("After Increment")
+//    println("$pi and $a")  // 3.14 and 1
+//}
+
+//fun main() {
+//    val pets = arrayOf("dog", "cat", "canary")
+//    println(java.util.Arrays.toString(pets))
+//
+//}
+
+//fun main() {
+//    val items = listOf("apple" ,"banana","Kiwifruit")
+//    for(items in items) {
+//        println(items)
+//
+////        o/p
+////        apple
+////        banana
+////        Kiwifruit
+//    }
+//}
+
+//fun main() {
+//    val items = setOf("apple","orange")
+//
+//    when {
+//        "apple" in items -> println("apple is their")
+//        "kiwi" in items -> println("Not their")
+//
+//        o/p
+//                apple is their
+//    }
+//}
+
+//fun main() {
+//    3 hypoteneuse 4
+//}
+//infix fun Int.hypoteneuse(breadth:Int) {
+//    var result = this * this + (breadth * breadth)
+//    println("The hypoteneuse is = $result")  // The hypoteneuse is = 25
+//}
+
